4f90eac62c1a411767d00d8f1387a1c445547262
Bala Nagendra <68959202+ybnagendra@users.noreply.github.com>
2020-10-12 23:11:16 +0530

    Update sheet1.py

--- sheet1.py
+++ sheet1.py
@@ -26,7 +26,7 @@ class KEYPAD:
 
         self.lastvalues = copy.deepcopy(self.values)
 
-        # close before open if used                                                                                                                 
+        # close before open if used
         for pin in self.a:
             try:
                 omega_gpio.closepin(pin)
@@ -69,75 +69,41 @@ class KEYPAD:
 
 
     def checkKey(self):
-        a = [0, 1, 2, 3, 11, 18, 8, 9]
-        r = [0, 1, 2, 3]
-        c = [18, 11, 8, 9]
-
-        key = [
-            ["1", "2", "3", "A"],
-            ["4", "5", "6", "B"],
-            ["7", "8", "9", "C"],
-            ["*", "0", "#", "D"],
-        ]
-
-        values = [
-            ["0", "0", "0", "0"],
-            ["0", "0", "0", "0"],
-            ["0", "0", "0", "0"],
-            ["0", "0", "0", "0"],
-            ["0", "0", "0", "0"],
-        ]
-
-        lastvalues = copy.deepcopy(values)
-
-        # close before open if used
-        for pin in a:
-            try:
-                omega_gpio.closepin(pin)
-            except:
-                e = 1  # dummy command :-)
-
-        # pin init
-        for pin in r:
-            omega_gpio.initpin(pin, 'out')
-
-        for pin in c:
-            omega_gpio.initpin(pin, 'in')
+        self.keypad_init()
 
         if True:
-            rpos = 0
-            for rpin in r:
-                omega_gpio.setoutput(r[0], 0)
-                omega_gpio.setoutput(r[1], 0)
-                omega_gpio.setoutput(r[2], 0)
-                omega_gpio.setoutput(r[3], 0)
+            for rpin in self.r:
+                omega_gpio.setoutput(self.r[0], 0)
+                omega_gpio.setoutput(self.r[1], 0)
+                omega_gpio.setoutput(self.r[2], 0)
+                omega_gpio.setoutput(self.r[3], 0)
                 omega_gpio.setoutput(rpin, 1)
                 time.sleep(0.05)
                 cpos = 0
-                for cpin in c:
+                for cpin in self.c:
                     input = omega_gpio.readinput(cpin)
-                    values[rpos][cpos] = input
+                    self.values[rpos][cpos] = input
                     cpos = cpos + 1
                 rpos = rpos + 1
 
             for x in range(0, 4):
                 for y in range(0, 4):
-                    if values[x][y] != lastvalues[x][y]:
-                        keycode = key[x][y]
-                        if values[x][y] == 1:
+                    if self.values[x][y] != self.lastvalues[x][y]:
+                        keycode = self.key[x][y]
+                        if self.values[x][y] == 1:
                             if keycode == 'D':
                                 print("D")
                             elif keycode == 'A':
                                 print("A")
                             else:
                                 print("Other")
-            lastvalues = copy.deepcopy(values)
+            lastvalues = copy.deepcopy(self.values)
 
             ####################################################################################
-            ####################################################################################                                                    
-if __name__=="__main__":                                                                                                                            
-        print("Hello")                                                                                                                              
-        k=KEYPAD()                                                                                                                                  
-        while True:                                                                                                                                 
-                key=k.getPressKey()                                                                                                                 
-                print(key)  
+            ####################################################################################
+if __name__=="__main__":
+        print("Hello")
+        k=KEYPAD()
+        while True:
+                key=k.getPressKey()
+                print(key)
