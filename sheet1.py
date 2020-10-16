e9b30509754d72aa5452544c6a6efeebc3ca815a
Bala Nagendra <68959202+ybnagendra@users.noreply.github.com>
2020-10-16 16:36:49 +0530

    Update sheet1.py

--- sheet1.py
+++ sheet1.py
@@ -1,43 +1,20 @@
-import time
-from OmegaExpansion import oledExp
+# import time
+# from OmegaExpansion import oledExp
 from dlmode import RUN_MODE
-from aaa1 import DL_MODES
+from modeselection import MODES
+from oledScreens import OLEDSCREENS
 
 
-def welcomeMessage():
-    oledExp.setVerbosity(0)
-    oledExp.driverInit()
-    oledExp.clear()
-    oledExp.setCursor(3, 0)
-    oledExp.write("Welcome to OnionOmega")
-    oledExp.setCursor(4, 0)
-    oledExp.write("Data Logger Project")
-    time.sleep(5)
-
-
-def displayModes():
-    oledExp.clear()
-    oledExp.setCursor(2, 0)
-    oledExp.write("Select mode(1,2,3):")
-    oledExp.setCursor(3, 0)
-    oledExp.write("1. CONFIGURE MODE")
-    oledExp.setCursor(4, 0)
-    oledExp.write("2. RUN MODE")
-    oledExp.setCursor(5, 0)
-    oledExp.write("3. BACKUP MODE")
-    oledExp.setCursor(6, 0)
-    oledExp.write("Mode of Operation:")
-
+k = MODES()         #mode selection through keypad
+disp = OLEDSCREENS()
 
 if __name__ == "__main__":
-    welcomeMessage()
-    timeCount1=0
+    disp.WelcomeMessage()
+    disp.ModesOfDataLogger()
+    userDelay1 = 0
+    del disp
     while True:
-        displayModes()
-        # Wait for user keypad input
-        DL_MODES.modeSelection()
-        oledExp.clear()
-        timeCount1=timeCount1+1
-        time.sleep(1)
-        if timeCount1==60:
+        k.modeSelection()           #Selection of mode through keypad
+        userDelay1 = userDelay1+1
+        if userDelay1 == 200:
             RUN_MODE()
