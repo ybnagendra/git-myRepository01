23ba74806cfd2b9cc0493a50d7b8af25e6b76920
Bala Nagendra <68959202+ybnagendra@users.noreply.github.com>
2020-11-19 12:22:48 +0530

    Create setdatetime.py

---
+++ setdatetime.py
@@ -0,0 +1,96 @@
+from OmegaExpansion import oledExp
+import datetime, time
+import serial
+
+# Open port with baud rate                                                                                                                          
+ser = serial.Serial(port="/dev/ttyS1", baudrate=115200, timeout=100)
+
+
+def check_date(year, month, day):
+    correctDate = None
+    try:
+        newDate = datetime.datetime(year, month, day)
+        correctDate = True
+    except ValueError:
+        correctDate = False
+    return correctDate
+
+
+class SET_DATE_AND_TIME:
+    def setDate(self):
+        while True:
+            print("class SET_DATE_AND_TIME  func setDate file setdatetime: ")
+            ser.write("Enter date in the format dd/mm/yy\r\n")
+            date_string = ser.read()
+
+            try:
+                self._dd = int(date_string[0:2])
+                self._mm = int(date_string[3:5])
+                self._yy = int(date_string[6:8]) + 2000
+                isvalid_date_string = check_date(self._yy, self._mm, self._dd)
+                lfd = len(date_string)
+
+                if isvalid_date_string == True and date_string[2] == '/' and date_string[5] == '/' and lfd == 8:
+                    break
+                elif isvalid_date_string == True and date_string[2] == '/' and date_string[5] == '/' and lfd != 8:
+                    oledExp.write("Format should be dd*mm*yy")
+                    time.sleep(2)
+                    oledExp.clear()
+                    print("Format should be dd/mm/yy")
+                elif isvalid_date_string == True and (date_string[2] != '/' or date_string[5] != '/'):
+                    oledExp.write("Format should be dd/mm/yy")
+                    time.sleep(2)
+                    oledExp.clear()
+                elif isvalid_date_string == False and (date_string[2] == '/' and date_string[5] == '/' and lfd == 8):
+                    oledExp.write("Invalid Date. ")
+                    time.sleep(2)
+                    oledExp.clear()
+
+            except:
+                oledExp.write("Format should be dd*mm*yy")
+                print("setDate except")
+                time.sleep(2)
+                oledExp.clear()
+        self.dateString = str(self._dd) + "-" + str(self._mm) + "-" + str(self._yy)
+
+    def setTime(self):
+        while True:
+            ser.write("Set time in the format hr:min:sec: ")
+            time_string = ser.read()
+
+            try:
+                self.hr = int(time_string[0:2])
+                self.min = int(time_string[3:5])
+                self.sec = int(time_string[6:8])
+
+                if 0 <= self.hr <= 24 and 0 <= self.min <= 59 and 0 <= self.sec <= 59:
+                    self.isvalid_ts = True
+                else:
+                    self.isvalid_ts = False
+
+                isvalid_time_string = self.isvalid_ts
+                len_of_time = len(time_string)
+
+                if isvalid_time_string == True and time_string[2] == '*' and time_string[5] == '*' and len_of_time == 8:
+                    break
+                elif isvalid_time_string == True and time_string[2] == '*' and time_string[
+                    5] == '*' and len_of_time != 8:
+                    oledExp.write("Format should be hr*min*sec Ex- 13*01*02")
+                    time.sleep(2)
+                    oledExp.clear()
+                    print("Format should be hr*min*sec")
+                elif isvalid_time_string == True and (time_string[2] != '*' or time_string[5] != '*'):
+                    oledExp.write("Format should be hr*min*sec Ex- 13*01*02")
+                    time.sleep(2)
+                    oledExp.clear()
+                elif isvalid_time_string == False and (
+                        time_string[2] == '*' and time_string[5] == '*' and len_of_time == 8):
+                    oledExp.write("Invalid Time. ")
+                    time.sleep(2)
+                    oledExp.clear()
+
+            except:
+                oledExp.write("Format should be hr:min:sec Ex- 13:01:02")
+                time.sleep(2)
+                oledExp.clear()
+        self.timeString = str(self.hr) + ":" + str(self.min) + ":" + str(self.sec)      
